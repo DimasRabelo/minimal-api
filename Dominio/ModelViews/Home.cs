38c9896a5c889634852e6ed485891e893e62cf09
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-09-28 08:10:45 -0300

    update-api

--- Dominio/ModelViews/Home.cs
+++ Dominio/ModelViews/Home.cs
@@ -2,6 +2,7 @@ namespace MinimalApi.Dominio.ModelViews;
 
 public struct Home
 {
-    public string Mensagem => "Bem vindo a API de Veículos - Minimal API";
-    public string Documentacao => "http://localhost:5179/swagger"; 
+    public string Mensagem { get => "Bem vindo a API de Veículos - Minimal API"; }
+
+    public string Documentacao { get => "/swagger"; }
 }
