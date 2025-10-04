c1124bedf1f1f2f8b9e2729bfff6c039eb818587
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-10-04 14:27:42 -0300

    Processo final do projeto de api de veiculos

---
+++ Dominio/DTOs/AdministradorDTO.cs
@@ -0,0 +1,12 @@
+
+
+namespace MinimalApi.DTOS;
+
+using MinimalApi.Dominio.Enuns; 
+
+public class AdministradorDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+    public Perfil? Perfil { get; set; } = default!;
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using MinimalApi.Dominio.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using MinimalApi.Dominio.ModelViews;
 using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Enuns;
 
 #region Builder
 
@@ -46,11 +47,58 @@ app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministra
         return Results.Unauthorized();
     }
 }).WithTags("Administradores");
+
+app.MapGet("/administradores", ([FromQuery] int pagina, IAdministradorServico administradorServico) =>
+{
+    return Results.Ok(administradorServico.Todos(pagina));
+}).WithTags("Administradores");
+
+app.MapGet("/Administradores/{id}", ([FromRoute] int id, IAdministradorServico administradorServico) =>
+{
+    var administrador = administradorServico.BuscaPorId(id);
+
+    if (administrador == null) return Results.NotFound();
+
+    return Results.Ok(administrador);
+
+}).WithTags("Administradores");
+
+
+app.MapPost("/administradores", ([FromBody] AdministradorDTO administradorDTO, IAdministradorServico administradorServico) =>
+{
+    var validacao = new ErrosDeValidacao {
+        Mensagens = new List<string>()
+    };
+
+    if (string.IsNullOrEmpty(administradorDTO.Email))
+        validacao.Mensagens.Add("O email não pode ser vazio");
+    if (string.IsNullOrEmpty(administradorDTO.Senha))
+        validacao.Mensagens.Add("A senha não pode ser vazia");
+    if (administradorDTO.Perfil == null)
+        validacao.Mensagens.Add("O Perfil não pode ser vazio");
+
+    if (validacao.Mensagens.Count > 0)
+        return Results.BadRequest(validacao);
+
+
+    
+    var administrador = new Administrador
+    {
+        Email = administradorDTO.Email,
+        Senha = administradorDTO.Senha,
+        Perfil = administradorDTO.Perfil?.ToString() ?? Perfil.editor.ToString()
+    };
+
+    administradorServico.Incluir(administrador);
+
+    return Results.Created($"/administrador/{administrador.Id}", administrador);
+
+}).WithTags("Administradores");
 #endregion
 
 #region Veiculos
 
- ErrosDeValidacao validaDTO(VeiculoDTO veiculoDTO)
+ErrosDeValidacao validaDTO(VeiculoDTO veiculoDTO)
 {
     var validacao = new ErrosDeValidacao {
         Mensagens = new List<string>()
@@ -152,6 +200,10 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+
+internal class administradorDTO
+{
+}
 #endregion
 
 
